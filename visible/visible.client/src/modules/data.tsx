00cc91de1d3b545864295981b248a467ef50369c
ahill-mtroyal <ahill389@mtroyal.ca>
2025-10-31 14:54:49 -0600

    added data handling module

---
+++ visible/visible.client/src/modules/data.tsx
@@ -0,0 +1,12 @@
+//This module contains all necessary functions for database interaction & data handling
+// fetch functions query the api and set react's state objects with the received data
+// Fetch functions expect a useState setter function to be passed
+
+
+
+/* All Gig Listings - Theoretically shouldn't be using this as there could be too many gig listings in the db to reasonably request*/
+export async function fetchAllGigListings(setter: any){
+    const response = await fetch('api/giglistings');
+    const data = await response.json();
+    setter(data);
+}
