a7932ced402d952e8229b9e381f19516a023ff84
acauf564 <acauf564@mtroyal.ca>
2025-11-01 19:27:36 -0600

    Added pre-commit support for prettier

--- visible/visible.client/src/App.tsx
+++ visible/visible.client/src/App.tsx
@@ -1,27 +1,26 @@
-import { useEffect, useState, createContext } from 'react';
-import './App.css';
-import { GigListings } from './modules/interfaces';
-import DevComponent from './components/DevComponent';
+import { useEffect, useState, createContext } from "react";
+import "./App.css";
+import { GigListings } from "./modules/interfaces";
+import DevComponent from "./components/DevComponent";
 
-export const Context = createContext<any>([])
+export const Context = createContext<any>([]);
 
 function App() {
-    //data state declarations
-    const [gigs, setGigs] = useState<GigListings[]>();
+  //data state declarations
+  const [gigs, setGigs] = useState<GigListings[]>();
 
-    //context object
-    const contextObj = {
-        gigs:[gigs, setGigs]
-    }
+  //context object
+  const contextObj = {
+    gigs: [gigs, setGigs],
+  };
 
-    return (
-        <main>
-            <Context.Provider value={contextObj}>
-                <DevComponent />
-            </Context.Provider>
-        </main>
-        
-    );
+  return (
+    <main>
+      <Context.Provider value={contextObj}>
+        <DevComponent />
+      </Context.Provider>
+    </main>
+  );
 }
 
 export default App;
