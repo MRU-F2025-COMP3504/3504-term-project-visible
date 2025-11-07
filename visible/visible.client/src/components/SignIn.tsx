fb7e84f81f4f044f4528927906c2206d8d837b48
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-07 00:02:53 -0700

    basic SignIn component made and added to DevComponent

--- visible/visible.client/src/components/SignIn.tsx
+++ visible/visible.client/src/components/SignIn.tsx
@@ -6,45 +6,66 @@ import {
   FieldLabel,
 } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { submitSignIn } from "@/modules/data";
 
 //This component implements the basic sign-in form html.
 //Documentation for the 'Field' components can be found here: https://ui.shadcn.com/docs/components/field
 //The form submits 2 fields 'Username' and 'Password' using a POST request to api/authentication/sign-in
 const SignIn = () => {
+  //field input states
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <FieldGroup className="border-2 border-indigo-600">
-      {/* Username Input */}
-      <Field>
-        <FieldLabel htmlFor="Username">Username</FieldLabel>
-        <Input
-          id="Username"
-          type="text"
-          placeholder="Enter your username."
-          required
-        />
-      </Field>
-      {/* Password Input */}
-      <Field>
-        <FieldLabel htmlFor="Password">Password</FieldLabel>
-        <Input id="Password" type="password" placeholder="********" required />
-        <FieldDescription>
-          Note: This project does not currently encrypt or hash username or
-          password. Excersize caution.
-        </FieldDescription>
-      </Field>
-      {/* Submit Button */}
-      <Field orientation="horizontal">
-        <Button
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(e);
-          }}
-        >
-          Sign In
-        </Button>
-      </Field>
-    </FieldGroup>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(`Sign in submitted with ${username} + ${password}`);
+        submitSignIn({
+          Username: username,
+          Password: password,
+        });
+      }}
+      className="border-2 border-red-400 p-4"
+    >
+      <FieldGroup className="">
+        {/* Username Input */}
+        <Field>
+          <FieldLabel htmlFor="Username">Username</FieldLabel>
+          <Input
+            id="Username"
+            type="text"
+            placeholder="Enter your username."
+            required
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
+          />
+        </Field>
+        {/* Password Input */}
+        <Field>
+          <FieldLabel htmlFor="Password">Password</FieldLabel>
+          <Input
+            id="Password"
+            type="password"
+            placeholder="********"
+            required
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <FieldDescription>
+            Note: This project does not currently encrypt or hash username and
+            password. Excersize caution.
+          </FieldDescription>
+        </Field>
+        {/* Submit Button */}
+        <Field orientation="horizontal">
+          <Button type="submit">Sign In</Button>
+        </Field>
+      </FieldGroup>
+    </form>
   );
 };
 
