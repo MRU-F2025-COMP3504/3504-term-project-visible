40021088e39564358ef8c9b1e41920e246e2ff93
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-06 21:49:22 -0700

    clearing preview content from DevComponent - no longer required, making space for other things

--- visible/visible.client/src/components/DevComponent.tsx
+++ visible/visible.client/src/components/DevComponent.tsx
@@ -6,62 +6,7 @@ import { fetchAllGigListings } from "@/modules/data";
 import { MouseEvent } from "react";
 
 const DevComponent = () => {
-  const [gigs, setGigs] = useContext(Context).gigs;
-
-  const contents =
-    gigs === undefined ? (
-      <p>
-        <em>
-          Loading... Please refresh once the ASP.NET backend has started. See{" "}
-          <a href="https://aka.ms/jspsintegrationreact">
-            https://aka.ms/jspsintegrationreact
-          </a>{" "}
-          for more details.
-        </em>
-      </p>
-    ) : (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr>
-            <th>Author</th>
-            <th>Description</th>
-            <th>Budget</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gigs.map((gig) => (
-            <tr key={gig.id}>
-              <td>{gig.author}</td>
-              <td>{gig.description}</td>
-              <td>{gig.budget}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-
-  useEffect(() => {
-    fetchAllGigListings(setGigs);
-  }, []);
-
-  return (
-    <div>
-      <h1 id="tableLabel">Recent Gigs</h1>
-      <p>This component demonstrates fetching data from the server.</p>
-      {contents}
-      <div className="bg-sky-800 m-4 p-2 rounded-md hover:bg-sky-600">
-        <p>If this div is blue, tailwind is working.</p>
-      </div>
-      <Button
-        onClick={(e: MouseEvent<HTMLButtonElement>) => {
-          e.preventDefault();
-          console.log("Button was pressed.");
-        }}
-      >
-        This is a button by shadcn
-      </Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default DevComponent;
