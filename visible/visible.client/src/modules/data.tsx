0b7a9685f5f88d7dbe751e203d334ce09c803429
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-07 02:29:45 -0700

    added sign up endpoint to data module

--- visible/visible.client/src/modules/data.tsx
+++ visible/visible.client/src/modules/data.tsx
@@ -3,21 +3,21 @@
 // Fetch functions expect a useState setter function to be passed
 
 //Port definition used for api paths
-const serverPort = "https://localhost:8080/";
+const SERVERPORT = "https://localhost:8080/";
 
-/* All Gig Listings - Theoretically shouldn't be using this as there could be too many gig listings in the db to reasonably request*/
+// All Gig Listings - Theoretically shouldn't be using this as there could be too many gig listings in the db to reasonably request
 export async function fetchAllGigListings(setter: any) {
   const response = await fetch(`${serverPort}api/giglistings`);
   const data = await response.json();
   setter(data);
 }
 
-/* Sign in request handler - function expects an object with 'Username' and 'Password' fields */
+// Sign in request handler - function expects an object with 'Username' and 'Password' fields
 //encrypt / hashing of user data should be added
 export async function submitSignIn(dataToSend) {
   console.log(`submitSignIn called with: ${JSON.stringify(dataToSend)}`);
   //Post request to server with given data
-  fetch(`${serverPort}api/authentication/sign-up`, {
+  fetch(`${SERVERPORT}api/authentication/sign-up`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -25,5 +25,18 @@ export async function submitSignIn(dataToSend) {
     body: JSON.stringify(dataToSend),
   }).then(() => {
     console.log(`fetch completed`);
+  });
+}
+
+// Sign up request handler - functions expects an object with 'Username' and 'Password' fields
+//encrypt / hashing of user data should be added
+export async function submitSignUp(dataToSend) {
+  //POST request to server with given data
+  fetch(`${SERVERPORT}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataToSend),
   });
 }
