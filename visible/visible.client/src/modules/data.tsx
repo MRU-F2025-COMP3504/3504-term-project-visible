cba11c96f301638233ee50b96f01556ab775e88e
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-07 00:02:00 -0700

    added sign in API endpoint to data module

--- visible/visible.client/src/modules/data.tsx
+++ visible/visible.client/src/modules/data.tsx
@@ -2,9 +2,28 @@
 // fetch functions query the api and set react's state objects with the received data
 // Fetch functions expect a useState setter function to be passed
 
+//Port definition used for api paths
+const serverPort = "https://localhost:8080/";
+
 /* All Gig Listings - Theoretically shouldn't be using this as there could be too many gig listings in the db to reasonably request*/
 export async function fetchAllGigListings(setter: any) {
-  const response = await fetch("api/giglistings");
+  const response = await fetch(`${serverPort}api/giglistings`);
   const data = await response.json();
   setter(data);
+}
+
+/* Sign in request handler - function expects an object with 'Username' and 'Password' fields */
+//encrypt / hashing of user data should be added
+export async function submitSignIn(dataToSend) {
+  console.log(`submitSignIn called with: ${JSON.stringify(dataToSend)}`);
+  //Post request to server with given data
+  fetch(`${serverPort}api/authentication/sign-up`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataToSend),
+  }).then(() => {
+    console.log(`fetch completed`);
+  });
 }
