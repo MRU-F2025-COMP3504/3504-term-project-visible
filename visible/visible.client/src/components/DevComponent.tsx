f41bb2fb93919b2af7e05e130ff5395d9441afde
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-07 21:51:26 -0700

    modified dev component to preview branch work

--- visible/visible.client/src/components/DevComponent.tsx
+++ visible/visible.client/src/components/DevComponent.tsx
@@ -4,62 +4,20 @@ import { Context } from "@/App";
 import { Button } from "@/components/ui/button";
 import { fetchAllGigListings } from "@/modules/data";
 import { MouseEvent } from "react";
+import GigListingCard from "./search/GigSearch/GigListingCard";
+import GigSearch from "./search/GigSearch/GigSearch";
 
 const DevComponent = () => {
   const [gigs, setGigs] = useContext(Context).gigs;
 
-  const contents =
-    gigs === undefined ? (
-      <p>
-        <em>
-          Loading... Please refresh once the ASP.NET backend has started. See{" "}
-          <a href="https://aka.ms/jspsintegrationreact">
-            https://aka.ms/jspsintegrationreact
-          </a>{" "}
-          for more details.
-        </em>
-      </p>
-    ) : (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr>
-            <th>Author</th>
-            <th>Description</th>
-            <th>Budget</th>
-          </tr>
-        </thead>
-        <tbody>
-          {gigs.map((gig) => (
-            <tr key={gig.id}>
-              <td>{gig.author}</td>
-              <td>{gig.description}</td>
-              <td>{gig.budget}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
-
+  //testing gig listings
   useEffect(() => {
     fetchAllGigListings(setGigs);
   }, []);
 
   return (
     <div>
-      <h1 id="tableLabel">Recent Gigs</h1>
-      <p>This component demonstrates fetching data from the server.</p>
-      {contents}
-      <div className="bg-sky-800 m-4 p-2 rounded-md hover:bg-sky-600">
-        <p>If this div is blue, tailwind is working.</p>
-      </div>
-      <Button
-        onClick={(e: MouseEvent<HTMLButtonElement>) => {
-          e.preventDefault();
-          console.log("Button was pressed.");
-        }}
-      >
-        This is a button by shadcn
-      </Button>
+      <GigSearch />
     </div>
   );
 };
