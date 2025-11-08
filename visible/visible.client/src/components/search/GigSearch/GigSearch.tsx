e5ec0f07c7c09f06afebd0fe03e1f0487e6443bc
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-07 18:03:20 -0700

    added skeleton for GigSearch component - parent component for the view used to search/filter gigs

---
+++ visible/visible.client/src/components/search/GigSearch/GigSearch.tsx
@@ -0,0 +1,27 @@
+import { Context } from "@/App";
+import { useContext, useState } from "react";
+import { BudgetRange } from "@/modules/interfaces";
+
+//This component implements the view for searching gig listings
+//It manages the state of the array used to build a list of gigs, then utilizes another component to produce that list
+const GigSearch = () => {
+  //grab gig context from broader app's context
+  const [gigs, setGigs] = useContext(Context).gigs;
+
+  //Search parameters
+  const [searchByName, setSearchByName] = useState("");
+  const [searchByDescription, setSearchByDescription] = useState("");
+  const [searchByBudget, setSearchByBudget] = useState<BudgetRange>({
+    low: 0,
+    high: Number.MAX_SAFE_INTEGER,
+  });
+
+  return (
+    <div>
+      {/* Filter Component */}
+      {/* List Component */}
+    </div>
+  );
+};
+
+export default GigSearch;
