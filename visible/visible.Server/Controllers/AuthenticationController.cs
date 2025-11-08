cf62f7bebbd21f7464c74003b53456dce6ebf857
acauf564 <112734799+acauf564@users.noreply.github.com>
2025-11-08 15:32:00 -0700

    Added backend implementation for JWT session management (#139)
    * Preliminary jwt token implementaion
    
    * Updated developer guide with JWT secret information

--- visible/visible.Server/Controllers/AuthenticationController.cs
+++ visible/visible.Server/Controllers/AuthenticationController.cs
@@ -1,4 +1,8 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using visible.Services.Interfaces;
 using visible.Services.Models;
 
@@ -12,15 +16,48 @@ namespace visible.Server.Controllers
     [Route("api/[controller]")]
     public class AuthenticationController(
         IAuthenticationRepository authenticationRepository,
-        ILogger<AuthenticationController> logger
+        ILogger<AuthenticationController> logger,
+        IConfiguration configuration
     ) : ControllerBase
     {
+        private IConfiguration _configuration => configuration;
+
         [HttpPost("sign-in")]
         public async Task<ActionResult> SignIn([FromBody] SignInRequest signInRequest)
         {
+            var username = signInRequest.Username;
             logger.LogInformation("Creating login request for {username}", signInRequest.Username);
 
-            return Ok(await authenticationRepository.SignInAsync(signInRequest));
+            var success = await authenticationRepository.SignInAsync(signInRequest);
+            if (!success)
+                return Unauthorized();
+            var token = GenerateJwtToken(signInRequest.Username);
+            return Ok(token);
+        }
+
+        private string GenerateJwtToken(string username)
+        {
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, username),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            };
+
+            var jwtKey = _configuration["jwtKey"];
+            var issuer = _configuration["jwtIssuer"];
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: issuer,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
 
         [HttpPost("sign-up")]
