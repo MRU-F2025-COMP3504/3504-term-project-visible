4a390dd2363acca162791da3a5add8849e71ed5e
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-09 17:18:48 -0700

    added note about redundant interface for gig attributes - to be condensed whenever the structure of the gig table is changed

--- visible/visible.client/src/modules/interfaces.tsx
+++ visible/visible.client/src/modules/interfaces.tsx
@@ -1,5 +1,9 @@
 //This module contains interfaces (object blueprints) used in data handling
 
+//TODO: I made 2 gig listing interfaces for some reason, the first is defining the exact structure received from the db,
+// the 2nd is defining the structure used while passing gig listings as props to components.
+// Condense these into 1 and update refences (whenever the gig structure is changed in the db).
+
 //GigListing Object - defines the data attatched to a unqiue gig listing
 export interface GigListings {
   id: number;
