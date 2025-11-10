6a97e2f6ab209004f534abec0e8b838eb652b2a0
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-10 00:43:05 -0700

    added SignIn/Up Modal components to landing page

--- visible/visible.client/src/components/LandingPage.tsx
+++ visible/visible.client/src/components/LandingPage.tsx
@@ -1,8 +1,29 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import SignInModal from "./SignInModal";
+import SignUpModal from "./SignUpModal";
+
 const LandingPage = () => {
   return (
-    <div>
+    <div className="border-2 border-white items-center justify-center gap-10 flex grow flex-col">
       {/* Visible Title Component */}
+      <h1>Visible</h1>
       {/* Sign In / Sign Up Buttons */}
+      <div>
+        {/* Sign Up */}
+        <SignInModal />
+        {/* Sign In */}
+        <SignUpModal />
+      </div>
     </div>
   );
 };
