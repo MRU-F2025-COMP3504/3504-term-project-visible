7c9a8d6fae2f1b62b04f5f3cb6efbf90364b64b0
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-09 23:11:37 -0700

    made skeleton for landing page

---
+++ visible/visible.client/src/components/LandingPage.tsx
@@ -0,0 +1,10 @@
+const LandingPage = () => {
+  return (
+    <div>
+      {/* Visible Title Component */}
+      {/* Sign In / Sign Up Buttons */}
+    </div>
+  );
+};
+
+export default LandingPage;
