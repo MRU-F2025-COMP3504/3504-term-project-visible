77cb8185fe512b907102808e227f1d0de0b36af3
acauf564 <acauf564@mtroyal.ca>
2025-11-10 23:22:29 -0700

    Updated GigListing tests to use new model

--- visible/visible.Tests/ControllerTests/GigListingsControllerTests.cs
+++ visible/visible.Tests/ControllerTests/GigListingsControllerTests.cs
@@ -8,25 +8,47 @@ namespace visible.Tests.ControllerTests;
 
 public class GigListingsControllerTests
 {
-    // [Fact]
-    // public async Task Test_CanGetGigListings()
-    // {
-    //     // Arrange
-    //     var mockRepository = new Mock<IGigListingRepository>();
-    //     var gigListings = new List<GigListing>
-    //     {
-    //         new GigListing(1, "Canela", "New Product Launch", 300),
-    //         new GigListing(2, "Breakaway Sports", "Instagram Follower Campaign", 1000),
-    //     };
-    //     mockRepository.Setup(r => r.GetRecentGigListings()).ReturnsAsync(gigListings);
-    //
-    //     // Act
-    //     var controller = new GigListingsController(mockRepository.Object);
-    //     var result = await controller.Get();
-    //
-    //     // Assert
-    //     Assert.IsType<OkObjectResult>(result);
-    //     var okResult = result as OkObjectResult;
-    //     Assert.NotNull(okResult);
-    // }
+    [Fact]
+    public async Task Test_CanGetGigListings()
+    {
+        // Arrange
+        var mockRepository = new Mock<IGigListingRepository>();
+        var gigListings = new List<GigListing>
+        {
+            new()
+            {
+                GigId = 1,
+                BusinessId = 1,
+                Title = "New Product Launch",
+                Description = "We want to hype up this new product on Instagram and YouTube.",
+                Location = "Calgary, AB",
+                Budget = 1500,
+                Requirements = "Minimum 1K followers on Instagram",
+                Status = "Open",
+                Deadline = new DateTime(2025, 11, 30, 19, 30, 00, DateTimeKind.Local),
+            },
+            new()
+            {
+                GigId = 2,
+                BusinessId = 2,
+                Title = "Spring into Savings",
+                Description = "Test description",
+                Location = "Edmonton, AB",
+                Budget = 750,
+                Requirements = "",
+                Status = "Open",
+                Deadline = new DateTime(2025, 12, 31, 19, 30, 00, DateTimeKind.Local),
+            },
+        };
+        mockRepository.Setup(r => r.GetRecentGigListings()).ReturnsAsync(gigListings);
+
+        // Act
+        var controller = new GigListingsController(mockRepository.Object);
+        var result = await controller.Get();
+
+        // Assert
+        Assert.IsType<OkObjectResult>(result);
+        var okResult = result as OkObjectResult;
+        Assert.NotNull(okResult);
+    }
 }
