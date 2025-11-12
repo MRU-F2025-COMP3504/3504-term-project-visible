ad8760e9f54612c3dda5c110554e357e02235a97
ahill-mtroyal <ahill389@mtroyal.ca>
2025-11-12 01:57:06 -0700

    added some styling to make searchlist scoll

--- visible/visible.client/src/components/CreateGigListingModal.tsx
+++ visible/visible.client/src/components/CreateGigListingModal.tsx
@@ -12,27 +12,37 @@ import {
 import CreateGigListingForm from "./CreateGigListingForm";
 import { useState } from "react";
 
-const CreateGigListingModal = () => {
+const CreateGigListingModal = ({ isParentOpen, isParentMenuHidden }) => {
   //Dialog (modal) open state
   const [isOpen, setIsOpen] = useState(false);
 
   //onSubmit -> passed to the GigListing Component to be appended onto the end of the form's submit function
   const onSubmit = () => {
     setIsOpen(false);
+    isParentMenuHidden(false);
+    isParentOpen(false);
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog
+      open={isOpen}
+      onOpenChange={(bool) => {
+        setIsOpen(bool);
+        isParentMenuHidden(bool);
+        isParentOpen(bool);
+      }}
+    >
       <DialogTrigger asChild>
         <Button
           variant="outline"
           onClick={(e) => {
             e.preventDefault();
+            isParentMenuHidden(true);
             //open the modal
             setIsOpen(true);
           }}
         >
-          Gig Listings
+          Post A Gig
         </Button>
       </DialogTrigger>
       <DialogContent className="bg-black">
