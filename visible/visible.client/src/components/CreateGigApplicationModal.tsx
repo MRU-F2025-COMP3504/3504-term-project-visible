db7afd36ad23f574c711ac899ad11a49d18af5f3
acauf564 <112734799+acauf564@users.noreply.github.com>
2025-11-12 12:39:52 -0700

    Implemented a modal form to apply to a gig listing (#164)
    * Updated Data module with recently created API endpoints
    
    * Implemented first draft of gig listing creation form
    
    * Added skeleton for influencer profile creation
    
    * updated internal GigStatus component to properly link form elements and state
    
    * added error checking for budget input ongig listing creation form
    
    * corrected weird behaviour within the Select status field
    
    * added TODO comment
    
    * returning app.tsx to landing page
    
    * modifying modal title, description, and comments
    
    * removing testing console logs & testing form submission to database
    
    * updating files names for gig list creation components
    
    * correcting search behaviour with updated gig model & added business name to search logic
    
    * removed console log testing messages from search functions
    
    * added a missing background to gig filter modal
    
    * Updated comments to reference the correct components (not sign-up)
    
    * Implemented basic gig application form
    
    * added application modal button to Gig listings
    
    * touched up the behaviour of the application form and modal
    
    * cleaning up landing page and app.tsx
    
    ---------
    
    Co-authored-by: ahill-mtroyal <ahill389@mtroyal.ca>

---
+++ visible/visible.client/src/components/CreateGigApplicationModal.tsx
@@ -0,0 +1,52 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { useState } from "react";
+import CreateGigApplicationForm from "./CreateGigApplicationForm";
+
+const CreateGigApplicationModal = ({ label, propGigId }) => {
+  //Dialog (modal) open state
+  const [isOpen, setIsOpen] = useState(false);
+
+  //onSubmit -> passed to the GigApplication Component to be appended onto the end of the form's submit function
+  const onSubmit = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button
+          variant="outline"
+          onClick={(e) => {
+            e.preventDefault();
+            //open the modal
+            setIsOpen(true);
+          }}
+        >
+          {label}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="bg-black">
+        <DialogTitle hidden={true}>Create Gig Listing Form</DialogTitle>
+        <DialogDescription hidden={true}>
+          Create a gig listing.
+        </DialogDescription>
+        <CreateGigApplicationForm
+          parentOnSubmit={onSubmit}
+          propGigId={propGigId}
+        />
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateGigApplicationModal;
