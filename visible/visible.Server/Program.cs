a6eff48a896d37fe6c0f64f57eeb93a040b7ffdf
acauf564 <acauf564@mtroyal.ca>
2025-11-29 11:08:03 -0700

    Updated repository lifetime from singleton to scoped

--- visible/visible.Server/Program.cs
+++ visible/visible.Server/Program.cs
@@ -25,16 +25,16 @@ var connectionString = builder.Configuration.GetConnectionString("PostgreDB") + 
 var jwtKey = builder.Configuration["jwtKey"];
 var jwtIssuer = builder.Configuration["jwtIssuer"];
 
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddSingleton<InitService>();
 builder.Services.AddScoped<IQueryBuilder, NpgsqlQueryBuilder>(p => new NpgsqlQueryBuilder(
     connectionString
 ));
-builder.Services.AddSingleton<InitService>();
-builder.Services.AddSingleton<IGigListingRepository, GigListingRepository>();
-builder.Services.AddSingleton<IAuthenticationRepository, AuthenticationRepository>();
-builder.Services.AddSingleton<IInfluencerRepository, InfluencerRepository>();
-builder.Services.AddSingleton<IBusinessRepository, BusinessRepository>();
-builder.Services.AddSingleton<IGigApplicationRepository, GigApplicationRepository>();
-builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddScoped<IGigListingRepository, GigListingRepository>();
+builder.Services.AddScoped<IAuthenticationRepository, AuthenticationRepository>();
+builder.Services.AddScoped<IInfluencerRepository, InfluencerRepository>();
+builder.Services.AddScoped<IBusinessRepository, BusinessRepository>();
+builder.Services.AddScoped<IGigApplicationRepository, GigApplicationRepository>();
 builder
     .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
